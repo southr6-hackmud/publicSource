4ee32d16b4af34ca233863a422952ad3762edcd7
jckirton <66995801+jckirton@users.noreply.github.com>
2026-09-04 22:27:58 +0930

    initial commit

---
+++ sync/sync.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$0")/.env"
+
+commit_and_push() {
+  cd "$(dirname "$0")/.." || exit 2
+
+  message='regular update'
+
+  if (($# > 0)); then
+    message=$1
+  fi
+
+  git commit -a -m "$message"
+  git push
+
+}
+
+copy() {
+  while IFS= read -r line; do
+    mkdir -p "$TARGET_COPY_DIR/$(dirname "$line")"
+    cp -R "$SCRIPT_SOURCE_DIR/$line" "$TARGET_COPY_DIR/$line"
+  done <"$SCRIPT_LIST"
+}
+
+copy
+commit_and_push "$@"
